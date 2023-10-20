d8f557eb888849be1a5d6103c38f1292d25b15b6
adnan007d <adnanmansuri54@gmail.com>
2023-10-20 16:33:31 +0530

    feat: tsconfig, started fileupload

--- cmd/aeom-image/main.go
+++ cmd/aeom-image/main.go
@@ -17,13 +17,25 @@ func renderTempl(c *fiber.Ctx, component templ.Component) error {
 func main() {
 	app := fiber.New()
 
-
-  app.Static("/css", "dist/css")
-  app.Static("/js", "dist/js")
+	app.Static("/css", "dist/css")
+	app.Static("/js", "dist/js")
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		component := views.Index("wtf")
 		return renderTempl(c, component)
+	})
+
+	app.Post("/images", func(c *fiber.Ctx) error {
+		files, err := c.MultipartForm()
+		if err != nil {
+			log.Printf("Error while decoding multiplat form: %v", err)
+		}
+		for _, file := range files.File["images"] {
+			log.Println(file.Filename)
+
+		}
+
+		return nil
 	})
 
 	log.Fatal(app.Listen(":6969"))
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Really wtf");
+const imageFileInput = document.getElementById("images")! as HTMLInputElement;
+
+imageFileInput.addEventListener("change", onInputImageChange);
+
+function onInputImageChange(_e: Event) {
+  const filesUL = document.getElementById("files")! as HTMLUListElement;
+  for (const file of imageFileInput.files!) {
+    const li = document.createElement("li");
+    li.textContent = `${file.name} - ${file.size} ${file.type}`;
+    filesUL.appendChild(li);
+  }
+  console.log(imageFileInput.files);
+}
