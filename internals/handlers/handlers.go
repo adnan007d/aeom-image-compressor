958cd1fe594e7d828c7a63b92b8a65feae2c8cfc
adnan007d <adnanmansuri54@gmail.com>
2023-10-27 20:24:10 +0530

    refactor: moving some code from handlers to util

--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -1,24 +1,18 @@
 package handlers
 
 import (
-	"bytes"
-	"image"
-	"io"
 	"io/fs"
 	"log"
-	"mime/multipart"
 	"os"
 	"strconv"
 	"sync"
 
 	"github.com/a-h/templ"
+	"github.com/adnan007d/aeom-image-compressor/internals/util"
 	"github.com/adnan007d/aeom-image-compressor/internals/views"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
-	"github.com/tidbyt/go-libwebp/webp"
 	_ "golang.org/x/image/webp"
-
-	"image/draw"
 	_ "image/jpeg"
 	_ "image/png"
 )
@@ -58,10 +52,10 @@ func CompressImages(c *fiber.Ctx) error {
 
 	for _, file := range form.File["images"] {
 		wg.Add(1)
-		go CompressImage(&wg, outChannel, CompressImageParams{
+		go util.CompressImage(&wg, outChannel, util.CompressImageParams{
 			File: file,
 			Dir:  dir,
-			CompressedImageOptions: CompressedImageOptions{
+			CompressedImageOptions: util.CompressedImageOptions{
 				Quality:   quality,
 				ImageType: "webp",
 			},
@@ -73,103 +67,10 @@ func CompressImages(c *fiber.Ctx) error {
 	close(outChannel)
 
 	var images []views.CompressedImagesType
-
 	for channelValue := range outChannel {
 		images = append(images, channelValue)
 	}
 
 	component := views.CompressedImages(images)
-	// component := views.CompressedImages([]views.CompressedImagesType{})
-
 	return renderTempl(c, component)
-
-}
-
-type CompressImageParams struct {
-	File                   *multipart.FileHeader
-	Dir                    string
-	CompressedImageOptions CompressedImageOptions
-}
-
-type CompressedImageOptions struct {
-	Quality   float32
-	ImageType string
-}
-
-func CompressImage(wg *sync.WaitGroup, images chan views.CompressedImagesType, params CompressImageParams) {
-	defer wg.Done()
-	originalFileName := params.Dir + "/original/" + params.File.Filename
-	compressedFileName := params.Dir + "/compressed/" + params.File.Filename + "." + params.CompressedImageOptions.ImageType
-
-	out, err := os.Create(originalFileName)
-	if err != nil {
-		log.Printf("Error while creating %s: %v", params.File.Filename, err)
-		return
-	}
-	defer out.Close()
-
-	openedFile, err := params.File.Open()
-	if err != nil {
-		log.Printf("Error while opening infile %s: %v", params.File.Filename, err)
-		return
-	}
-	defer openedFile.Close()
-
-	var inMemoryImage bytes.Buffer
-
-	multiWriter := io.MultiWriter(&inMemoryImage, out)
-
-	io.Copy(multiWriter, openedFile)
-
-	// Decoding Image to standard format
-	decodedImage, _, err := image.Decode(&inMemoryImage)
-	if err != nil {
-		log.Printf("Error decoding image %s: %v", originalFileName, err)
-		return
-	}
-
-	// log.Printf("%v is %v", originalFileName, iType)
-
-	decodedOutFile, err := os.Create(compressedFileName)
-	if err != nil {
-		log.Printf("Error Opening outfile %s: %v", compressedFileName, err)
-		return
-	}
-	defer decodedOutFile.Close()
-
-	var convertError error = nil
-
-	switch params.CompressedImageOptions.ImageType {
-	case "webp":
-		convertError = ConvertToWebp(decodedOutFile, decodedImage, params.CompressedImageOptions)
-
-	default:
-		log.Println("File Not supported")
-	}
-
-	if convertError != nil {
-		log.Printf("Error while encoding image %s: %v", compressedFileName, convertError)
-	}
-
-	images <- views.CompressedImagesType{
-		Source: views.ImageType{Src: originalFileName},
-		Dest:   views.ImageType{Src: compressedFileName},
-	}
-}
-
-func ConvertToWebp(w io.Writer, srcImage image.Image, options CompressedImageOptions) error {
-	newImage := image.NewRGBA(srcImage.Bounds())
-
-	draw.Draw(newImage, srcImage.Bounds(), srcImage, srcImage.Bounds().Min, draw.Src)
-
-	config, err := webp.ConfigPreset(webp.PresetDefault, options.Quality)
-	if err != nil {
-		return err
-	}
-
-	err = webp.EncodeRGBA(w, newImage, config)
-	if err != nil {
-		return err
-	}
-	return nil
 }
---
+++ internals/util/util.go
@@ -0,0 +1,103 @@
+package util
+
+import (
+	"bytes"
+	"image"
+	"image/draw"
+	"io"
+	"log"
+	"mime/multipart"
+	"os"
+	"sync"
+
+	"github.com/adnan007d/aeom-image-compressor/internals/views"
+	"github.com/tidbyt/go-libwebp/webp"
+)
+
+
+type CompressImageParams struct {
+	File                   *multipart.FileHeader
+	Dir                    string
+	CompressedImageOptions CompressedImageOptions
+}
+
+type CompressedImageOptions struct {
+	Quality   float32
+	ImageType string
+}
+
+func CompressImage(wg *sync.WaitGroup, images chan views.CompressedImagesType, params CompressImageParams) {
+	defer wg.Done()
+	originalFileName := params.Dir + "/original/" + params.File.Filename
+	compressedFileName := params.Dir + "/compressed/" + params.File.Filename + "." + params.CompressedImageOptions.ImageType
+
+	out, err := os.Create(originalFileName)
+	if err != nil {
+		log.Printf("Error while creating %s: %v", params.File.Filename, err)
+		return
+	}
+	defer out.Close()
+
+	openedFile, err := params.File.Open()
+	if err != nil {
+		log.Printf("Error while opening infile %s: %v", params.File.Filename, err)
+		return
+	}
+	defer openedFile.Close()
+
+  // NOTE: Maybe omit in memory and use the freshly written file?
+  // Creating a writer to write to make a copy in memory of the image and write to disk
+	var inMemoryImage bytes.Buffer
+	multiWriter := io.MultiWriter(&inMemoryImage, out)
+	io.Copy(multiWriter, openedFile)
+
+	// Decoding Image to standard format
+	decodedImage, _, err := image.Decode(&inMemoryImage)
+	if err != nil {
+		log.Printf("Error decoding image %s: %v", originalFileName, err)
+		return
+	}
+
+	decodedOutFile, err := os.Create(compressedFileName)
+	if err != nil {
+		log.Printf("Error Opening outfile %s: %v", compressedFileName, err)
+		return
+	}
+	defer decodedOutFile.Close()
+
+	var convertError error = nil
+
+	switch params.CompressedImageOptions.ImageType {
+	case "webp":
+		convertError = ConvertToWebp(decodedOutFile, decodedImage, params.CompressedImageOptions)
+
+	default:
+		log.Println("File Not supported")
+	}
+
+	if convertError != nil {
+		log.Printf("Error while encoding image %s: %v", compressedFileName, convertError)
+	}
+
+	images <- views.CompressedImagesType{
+		Source: views.ImageType{Src: originalFileName},
+		Dest:   views.ImageType{Src: compressedFileName},
+	}
+}
+
+func ConvertToWebp(w io.Writer, srcImage image.Image, options CompressedImageOptions) error {
+	newImage := image.NewRGBA(srcImage.Bounds())
+
+	draw.Draw(newImage, srcImage.Bounds(), srcImage, srcImage.Bounds().Min, draw.Src)
+
+	config, err := webp.ConfigPreset(webp.PresetDefault, options.Quality)
+	if err != nil {
+		return err
+	}
+
+	err = webp.EncodeRGBA(w, newImage, config)
+	if err != nil {
+		return err
+	}
+	return nil
+}
